600a543235d2403ed45b786161cfef849b781b90
cr545l_xps <cr545l@gmail.com>
2018-06-21 06:43:26 +0900

    리팩토링

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-# parser.py
 import requests
 from bs4 import BeautifulSoup as bs
 import time
 import operator
 
-from multiprocessing import Pool # Pool import하기
+from multiprocessing import Pool
 
 def get_html(url):
-   _html = ""
+   result = ""
    resp = requests.get(url)
    if resp.status_code == 200:
-      _html = resp.text
-   return _html
+      result = resp.text
+   return result
 
 def question_title(soup):
-    all_divs = soup.find("meta",  itemprop="title name")
-    return all_divs["content"]
+    meta = soup.find("meta",  itemprop="title name")
+    if meta is not None:
+        return meta["content"]
+    else:
+        return ""
     
 def question_body(soup):
-    all_divs = soup.find_all("div",{'class':'postcell post-layout--right'})
-    for i in all_divs:
-        all_divs2 = i.find_all("p")
-        for name in all_divs2:
-            return name.get_text()
+    postcell = soup.find_all("div",{'class':'postcell post-layout--right'})
+    for i in postcell:
+        div_p = i.find_all("p")
+        for name in div_p:
+            return str(name.get_text())
 
 def answer(soup):
-    all_divs = soup.find_all("div",{'class':'answercell post-layout--right'})
-    for i in all_divs:
-        all_divs2 = i.find_all("p")
-        for name in all_divs2:
+    answercell = soup.find_all("div",{'class':'answercell post-layout--right'})
+    for i in answercell:
+        div_p = i.find_all("p")
+        for name in div_p:
             return name.get_text()        
 
 def comments(soup):
@@ -36,38 +38,41 @@ def comments(soup):
     for comment in comments:
         return comment.get_text()
 
-
 def crawler(dic, index):
     value = get_html("https://stackoverflow.com/questions/"+str(index))
     soup = bs(value, 'html.parser')
 
-    result = ""
-    result += question_title(soup)
-    result += '\n'
-    result += question_body(soup)
-    result += '\n'
-    result += answer(soup)
-    result += '\n'
-    result += comments(soup)
+    if soup is not None:
+        result = ""
+        result += str(question_title(soup))
+        result += '\n'
+        result += str(question_body(soup))
+        result += '\n'
+        result += str(answer(soup))
+        result += '\n'
+        result += str(comments(soup))
 
-    for i in result.split():
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
+        for i in result.split():
+            key = i.lower()
+            if key in dic:
+                dic[key] += 1
+            else:
+                dic[key] = 1
 
-dic = {}
+dic = {}    
 
-for i in range(7782772, 7782776):
-    crawler(dic,7782772)
+start = 7783772
+length = 100000
 
- #dic = sorted(dic.items(), key=operator.itemgetter(1), reverse=True)
+target = range(start, start+length)
+for index, val in enumerate(target):    
+    print(str(index+1) +"/"+str(len(target)))
+    crawler(dic,val)
+    time.sleep(1)
+
+with open('result.txt', 'w', encoding='UTF-8') as f:
+    for key in dic.keys():
+        f.write(key+ "\t"+str(dic[key] )+"\n")
+    f.close()
 
 print(dic)
-
-f = open("result.txt", 'w')
-
-for key in dic.keys():
-    f.write(key+ "\t"+str(dic[key] )+"\n")
-
-f.close()
